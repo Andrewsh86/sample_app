a3c4b95a4098c1386e33af0dbb04b070547fc73f
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-10-23 21:12:42 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -26,6 +27,10 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def feed
+    Micropost.where('user_id = ?', id)
   end
 
   def forget
